6a2017c17dbd145e330021d397145e2ab488342c
teerawat-cc <terawat.cc@gmail.com>
2023-03-13 02:13:03 +0700

    second commit

--- 00_pytorch_fundamentals/operation.py
+++ 00_pytorch_fundamentals/operation.py
@@ -95,17 +95,67 @@ print(output)
 print(f"\nOutput shape: {output.shape}")
 
 # find min, max, mean, sum, etc (tensor aggregation)
-x = torch.arange(0, 100, 10) # long dtype
+x = torch.arange(0, 100, 10) # x.dtype is long
 print(x)
 
 # find min
-print(torch.min(x), x.min())
+print(f"Minimum: {x.min(), torch.min(x)}")
 
 # find max
-print(torch.max(x), x.max())
+print(f"Maximum: {x.max(), torch.max(x)}")
 
 # find mean
-print(torch.mean(x.type(torch.float32)), x.type(torch.float32).mean())
-# RuntimeError: mean(): could not infer output dtype. 
-# Input dtype must be either "a floating point" or "complex" dtype. Got: Long
+print(f"Mean: {torch.mean(x.type(torch.float32)), x.type(torch.float32).mean()}")
+# won't work without float or complex datatype because x.dtype is long
 
+# RuntimeError: mean(): could not infer output dtype. 
+# Input dtype must be either a floating point or complex dtype. Got: Long
+
+# find sum
+print(f"Sum: {x.sum(), torch.sum(x)}")
+
+# find positional min/max #data is position of x
+print(f"Index where min value occurs: {x.argmin()} value is", x[x.argmin()])
+print(f"Index where max value occurs: {x.argmax()} value is", x[x.argmax()])
+
+
+# Reshaping, stacking, squeezing and unsqueezing tensors
+# • Reshaping - reshapes an input tensor to a defined shape
+# • View - Return a view of an input tensor of certain shape but keep the same memory as the original tensor
+# • Stacking - combine multiple tensors on top of each other (vstack) or side by side (hstack)
+# • Squeeze - removes all 1 dimensions from a tensor
+# • Unsqueeze - add a 1 dimension to a target tensor
+# • Permute - Return a view of the input with dimensions permuted (swapped) in a certain way
+
+x = torch.arange(1., 10., 1.)
+print(x,x.shape)
+
+# Add an extra dimension
+x_reshaped = x.reshape(9, 1) # layer outer, layer inner
+print(x_reshaped, x_reshaped.shape)
+
+x_reshaped = x.reshape(1, 9)  # layer outer, layer inner
+print(x_reshaped, x_reshaped.shape)
+
+# Change the view
+z = x.view(1, 9)
+print(z, z.shape)
+
+# changing z changes x (because input tensor of certain shape but keep the same memory as the original tensor)
+z[:, 0]= 5
+print(z,x)
+print("\n")
+
+# stack tensors on top of each other
+x_stracked = torch.stack([x, x, x, x], dim=0)
+print(x_stracked)
+x_stracked = torch.stack([x, x, x, x], dim=1)
+print(x_stracked)
+print("\n")
+
+x_stracked = torch.hstack([x, x], out=None)
+print(x_stracked)
+x_stracked = torch.vstack([x, x], out=None)
+print(x_stracked)
+
+#page 25 bye bye sleep well
