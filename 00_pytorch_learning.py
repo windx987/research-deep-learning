65fe2fd9aa03d9960825c389b0ce1a568aa855eb
teerawat-cc <terawat.cc@gmail.com>
2023-03-12 16:12:00 +0700

    Second Commit

--- 00_pytorch_learning.py
+++ 00_pytorch_learning.py
@@ -2,4 +2,12 @@ import torch
 import pandas   as pd
 import numpy    as np
 import matplotlib.pyplot as plt
+
 print(torch.__version__)
+print(torch.cuda.is_available())
+
+## Introduction of Tensors 
+
+#scalar
+scalar  = torch.tensor(7)
+print(scalar)
