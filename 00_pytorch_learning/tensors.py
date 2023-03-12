2760935f5643ca82afd5719857830713030f27c1
teerawat-cc <terawat.cc@gmail.com>
2023-03-12 20:23:20 +0700

    Second Commit

--- 00_pytorch_learning/tensors.py
+++ 00_pytorch_learning/tensors.py
@@ -39,9 +39,33 @@ print(TENSOR[0][1][2])
 random_tensor = torch.rand(3, 4) #torch.rand(4, 4, 4)
 print(random_tensor)
 
-#create random tensor of [colour_channels, height, width]
-random_image_size_tensor = torch.rand(size=(3, 224, 224))
+#create random tensor of [height, width, color_channels]
+random_image_size_tensor = torch.rand(size=(224, 224, 3)) #defult is size=
 print(random_image_size_tensor.shape,random_image_size_tensor.ndim)
 print(random_image_size_tensor[0][1])
 
-#
+#zero and one
+zeros = torch.zeros(3, 4)
+print(zeros, zeros.dtype)
+
+ones = torch.ones(3, 4)
+print(ones, ones.dtype)
+
+print(ones*random_tensor)
+print(zeros*random_tensor)
+
+#create range and tensors-like
+one_to_ten = torch.arange(0,10,1) # step=, end=, step= defult is 1
+print(one_to_ten)
+
+#create tensors like
+ten_zeros = torch.zeros_like(input=one_to_ten)
+print(ten_zeros)
+
+#tensor datatypes
+
+float_32_tensor = torch.tensor([3.0, 6.0, 9.0],
+                               dtype=None,
+                               device=None,
+                               requires_grad=False)
+print(float_32_tensor.shape, float_32_tensor.dtype, float_32_tensor.device)
