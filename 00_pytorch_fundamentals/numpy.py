e42c5c7b1ceb8b40aa899d72c31722d003f2dc56
teerawat-cc <terawat.cc@gmail.com>
2023-03-13 12:20:37 +0700

    first commit

--- 00_pytorch_fundamentals/numpy.py
+++ 00_pytorch_fundamentals/numpy.py
@@ -1,6 +1,7 @@
 # NumPy array to tensor
 import torch
 import numpy as np
-array = np.arange(1.0, 8.0)
+
+array = np.array(1.0, 8.0)
 tensor = torch.from_numpy(array)
-array, tensor
+print(array, tensor)
