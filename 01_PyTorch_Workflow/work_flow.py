59831a3e7126eeb6973a4787886d51e766755a61
teerawat-cc <terawat.cc@gmail.com>
2023-03-13 20:30:23 +0700

    second commit

---
+++ 01_PyTorch_Workflow/work_flow.py
@@ -0,0 +1,9 @@
+what_were_covering = {
+    1: "data (prepare and load)",
+    2: "build model",
+    3: "fitting the model to data (training)",
+    4: "making predictions and evaluating a model (inference)",
+    5: "saving and loading a model",
+    6: "putting it all together"
+}
+
