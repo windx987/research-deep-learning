e7e8d7eddd6a65bb9659e8bf4426751eeb8eb0be
teerawat-cc <terawat.cc@gmail.com>
2023-03-14 09:51:24 +0700

    firsst commit

---
+++ split_data.py
@@ -0,0 +1,59 @@
+## pytorch workflow
+what_were_covering = {  1: "data (prepare and load)",
+                        2: "build model",
+                        3: "fitting the model to data (training)",
+                        4: "making predictions and evaluating a model (inference)",
+                        5: "saving and loading a model",
+                        6: "putting it all together"
+                        }
+
+import torch
+from torch import nn # nn contains all of PyTorch's building blocks for neural networks
+import matplotlib.pyplot as plt
+
+# Create *known* parameters
+weight = 0.7
+bias = 0.3
+
+# Create data
+start = 0
+end = 1
+step = 0.02
+X = torch.arange(start, end, step).unsqueeze(dim=1)
+y = weight * X + bias
+
+X[:10], y[:10]
+
+# Create train/test split
+train_split = int(0.8 * len(X)) # 80% of data used for training set, 20% for testing 
+X_train, y_train = X[:train_split], y[:train_split]
+X_test, y_test = X[train_split:], y[train_split:]
+
+print(len(X_train), len(y_train), len(X_test), len(y_test))
+
+def plot_predictions(train_data=X_train, 
+                     train_labels=y_train, 
+                     test_data=X_test, 
+                     test_labels=y_test, 
+                     predictions=None):
+    
+    """Plots training data, test data and compares predictions."""
+    
+    plt.figure(figsize=(10, 7))
+
+    # Plot training data in blue
+    plt.scatter(train_data, train_labels, c="b", s=4, label="Training data")
+  
+    # Plot test data in green
+    plt.scatter(test_data, test_labels, c="g", s=4, label="Testing data")
+
+    if predictions is not None:
+        # Plot the predictions in red (predictions were made on the test data)
+        plt.scatter(test_data, predictions, c="r", s=4, label="Predictions")
+
+    # Show the legend
+    plt.legend(prop={"size": 14});
+    plt.show()
+
+plot_predictions()
+
