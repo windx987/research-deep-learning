b9955a7a649489f9d22f534edf2d685fd3fcd066
teerawat-cc <terawat.cc@gmail.com>
2023-03-18 01:01:05 +0700

    second commit

--- Learn PyTorch/02_PyTorch_Neural_Classification/non_linear_activation.py
+++ Learn PyTorch/02_PyTorch_Neural_Classification/non_linear_activation.py
@@ -6,16 +6,31 @@ from sklearn.datasets import make_circles
 from sklearn.model_selection import train_test_split
 from helper_function import plot_predictions, plot_decision_boundary
 
+# Replicating non-linear activation functions
+
 # Create a tensor
 A = torch.arange(-10, 10, 1, dtype=torch.float32)
-print(A.dtype)
-print(A)
+print(A, A.dtype)
 
-# Visualize the tensor
-plt.plot(A)
-plt.show()
+# plt.plot(A)
+# plt.show()
+
+def relu(x: torch.tensor) -> torch.tensor:
+    return torch.maximum(torch.tensor(0), x)
+
+print(relu(A))
 
 plt.plot(torch.relu(A))
 plt.show()
 
-print(A)
+def sigmoid(x):
+    return 1 / (1 + torch.exp(-x))
+
+print(sigmoid(A))
+
+plt.plot(sigmoid(A));
+plt.show()
+
+plt.plot(torch.sigmoid(A)); 
+plt.show()
+
