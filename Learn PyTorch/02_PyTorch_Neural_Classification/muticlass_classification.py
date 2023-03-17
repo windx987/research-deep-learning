b935b83548c2be1fe95bd6c791110b9e341c0b6c
teerawat-cc <terawat.cc@gmail.com>
2023-03-18 01:48:45 +0700

    second commit

--- Learn PyTorch/02_PyTorch_Neural_Classification/muticlass_classification.py
+++ Learn PyTorch/02_PyTorch_Neural_Classification/muticlass_classification.py
@@ -1,4 +1,5 @@
 import torch
+from torch import nn
 import matplotlib.pyplot as plt
 from sklearn.datasets import make_blobs
 from sklearn.model_selection import train_test_split
@@ -24,8 +25,33 @@ X_blob_train, X_blob_test, y_blob_train, y_blob_test = train_test_split(X_blob,
                                                                         y_blob,
                                                                         test_size=0.2,
                                                                         random_state=RANDOM_SEED)
-
 # 4. Plot data (visualize, visualize, visualize)
 plt.figure(figsize=(10, 7))
 plt.scatter(X_blob[:, 0], X_blob[:, 1], c=y_blob, cmap=plt.cm.RdYlBu)
-plt.show()
+# plt.show()
+
+# Make device agnostic code
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print(device)
+
+class BlobModel(nn.Module):
+    def __init__(self, input_features, output_features, hidden_units=8):
+        super().__init__()
+        self.linear_layer_stack = nn.Sequential(
+            nn.Linear(in_features=input_features, out_features=hidden_units),
+            nn.Linear(in_features=hidden_units, out_features=hidden_units),
+            nn.Linear(in_features=hidden_units, out_features=output_features)
+        )
+    def forward(self, x):
+        return self.linear_layer_stack(x)
+    
+model_4 = BlobModel(input_features=2, output_features=4, hidden_units=8).to(device)
+print(model_4)
+
+print(X_blob_train.shape, y_blob_train[:5])
+print(torch.unique(y_blob_train))
+
+
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(params=model_4.parameters(), lr=0.1)
+
