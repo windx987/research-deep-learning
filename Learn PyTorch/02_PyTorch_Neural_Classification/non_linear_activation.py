11e0bc7bd19c6ad81033bbea6e255282a1e8143f
teerawat-cc <terawat.cc@gmail.com>
2023-03-19 01:25:29 +0700

    second commit

--- Learn PyTorch/02_PyTorch_Neural_Classification/non_linear_activation.py
+++ Learn PyTorch/02_PyTorch_Neural_Classification/non_linear_activation.py
@@ -1,9 +1,13 @@
-import matplotlib.pyplot as plt
-import pandas as pd
 import torch
 from torch import nn
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
 from sklearn.datasets import make_circles
 from sklearn.model_selection import train_test_split
+
 from helper_function import plot_predictions, plot_decision_boundary
 
 # Replicating non-linear activation functions
@@ -34,3 +38,18 @@ plt.show()
 plt.plot(torch.sigmoid(A)); 
 plt.show()
 
+tensor_A = torch.arange(-100, 100, 1)
+plt.plot(tensor_A)
+plt.show()
+
+def tanh(z):
+	return (np.exp(z) - np.exp(-z)) / (np.exp(z) + np.exp(-z))
+
+plt.plot(tanh(tensor_A))
+plt.show()
+
+plt.plot(torch.tanh(tensor_A))
+plt.show()
+
+# https://youtube.com/watch?v=Fu273ovPBmQ
+# https://en.wikipedia.org/wiki/Activation_function#Table_of_activation_functions
