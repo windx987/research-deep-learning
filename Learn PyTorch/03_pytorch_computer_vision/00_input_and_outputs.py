3e1b3d87a46fedcde3415c00d206c44ac3bbb245
teerawat-cc <terawat.cc@gmail.com>
2023-03-19 23:09:54 +0700

    second commit

--- Learn PyTorch/03_pytorch_computer_vision/00_input_and_outputs.py
+++ Learn PyTorch/03_pytorch_computer_vision/00_input_and_outputs.py
@@ -3,7 +3,7 @@ import torch
 from torch import nn
 
 # Import helper functions
-from helper_function import plot_predictions, plot_decision_boundary
+from helper_function import plot_predictions, plot_decision_boundary, accuracy_fn
 
 # Import torchvision 
 import torchvision
@@ -19,6 +19,10 @@ import matplotlib.pyplot as plt
 # Check versions
 # Note: your PyTorch version shouldn't be lower than 1.10.0 and torchvision version shouldn't be lower than 0.11
 print(f"PyTorch version: {torch.__version__}\ntorchvision version: {torchvision.__version__}")
+
+# Setup device agnostic code
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print(f"Using device: {device}")
 
 # use dataset FASHIONMNIST
 
@@ -128,15 +132,37 @@ out = flatten_model(x)
 # Print out what happened
 print(f"Shape before flattening: {x.shape} -> [color_channels, height, width]")
 print(f"Shape after flattening: {out.shape} -> [color_channels, height*width]")
-print(out.squeeze().shape())
+print(out.squeeze().shape)
 
 class FashionMNISTModelV0(nn.Module):
     def __init__(self, input_shape:int, hidden_uints:int, output_shape:int):
         super().__init__()
         self.layer_stack = nn.Sequential(
             nn.Flatten(),
-            nn.Linear(input_shape=input_shape, hidden_uints=hidden_uints),
+            nn.Linear(in_features=input_shape, out_features=hidden_uints),
             nn.Linear(in_features=hidden_uints, out_features=output_shape)
         )
     def forward(self, x):
         return self.layer_stack(x)
+
+torch.manual_seed(42)
+
+model_0 = FashionMNISTModelV0(
+    input_shape=28*28, # this is 784
+    hidden_uints=10, # how many uints in hidden layer
+    output_shape=len(class_names) # how many uints in output layer
+).to("cpu")
+
+print(model_0)
+
+# dummy input
+dummy_x = torch.rand([1, 1, 28, 28])
+print(dummy_x.shape)
+
+print(model_0(dummy_x).shape)
+print(model_0.state_dict())
+
+# Setup loss function and optimizer
+loss_fn = nn.CrossEntropyLoss()  # multi-class data, loss function will be nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(params=model_0.parameters(), # (stochastic gradient descent)
+                            lr=0.1)
