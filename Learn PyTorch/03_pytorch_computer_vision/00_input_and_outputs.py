386748df5949f99cfdee92b3de56793009f10d82
teerawat-cc <terawat.cc@gmail.com>
2023-03-19 19:48:24 +0700

    datasets downloaded

--- Learn PyTorch/03_pytorch_computer_vision/00_input_and_outputs.py
+++ Learn PyTorch/03_pytorch_computer_vision/00_input_and_outputs.py
@@ -5,6 +5,7 @@ from torch import nn
 # Import torchvision 
 import torchvision
 from torchvision import datasets
+from torchvision import transforms
 from torchvision.transforms import ToTensor
 
 # Import matplotlib for visualization
@@ -13,4 +14,26 @@ import matplotlib.pyplot as plt
 # Check versions
 # Note: your PyTorch version shouldn't be lower than 1.10.0 and torchvision version shouldn't be lower than 0.11
 print(f"PyTorch version: {torch.__version__}\ntorchvision version: {torchvision.__version__}")
+
+# use dataset FASHIONMNIST
+
+# Setup training data
+train_data = datasets.FashionMNIST(
+    root="data", # where to download data to?
+    train=True, # do we want the training dataset?
+    download=True, # do we want to download yes/no?
+    transform=transforms.ToTensor(), # how do we want to transform the data?
+    target_transform=None # how do we want to transform the labels/targets?
+)
+
+test_data = datasets.FashionMNIST(
+    root="data", # where to download data to?
+    train=False, # do we want the train or test dataset?
+    download=True, # do we want to download yes/no?
+    transform=transforms.ToTensor(), # how do we want to transform the data?
+    target_transform=None # how do we want to transform the labels/targets?
+)
+
+# print data
+print(train_data, test_data)
 
