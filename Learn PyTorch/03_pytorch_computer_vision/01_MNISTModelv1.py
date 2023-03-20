cb804a4921715d6b1d71d68b9252608caf4f44a1
teerawat-cc <terawat.cc@gmail.com>
2023-03-20 23:35:11 +0700

    second commit

--- Learn PyTorch/03_pytorch_computer_vision/01_MNISTModelv1.py
+++ Learn PyTorch/03_pytorch_computer_vision/01_MNISTModelv1.py
@@ -21,10 +21,6 @@ import matplotlib.pyplot as plt
 
 from timeit import default_timer as timer
 
-# Check versions
-# Note: your PyTorch version shouldn't be lower than 1.10.0 and torchvision version shouldn't be lower than 0.11
-print(f"PyTorch version: {torch.__version__}\ntorchvision version: {torchvision.__version__}")
-
 def print_train_time(start: float,
                      end: float, 
                      device: torch.device = None):
@@ -52,7 +48,6 @@ test_data = datasets.FashionMNIST(
     target_transform=None # how do we want to transform the labels/targets?
 )
 
-# print data
 print(train_data, test_data)
 print(len(train_data), len(test_data))
 
@@ -68,10 +63,6 @@ class_to_idx = train_data.class_to_idx
 print(f"Image shape: {image.shape} -> [color_channels, height, width]") 
 print(f"Image label: {class_names[label]}")
 
-# visualize the image
-image, label = train_data[0]
-print(f"\nImage shape: {image.shape}")
-
 # Setup the batch size hyperparameter
 BATCH_SIZE = 32
 
@@ -82,23 +73,20 @@ test_dataloader = DataLoader(dataset=test_data,
                             batch_size=BATCH_SIZE,
                             shuffle=False)
 
-# Let's check out what what we've created
 print(f"DataLoder:{train_dataloader, test_dataloader}")
 print(f"Lenght:{len(train_dataloader)} batches of {BATCH_SIZE}...")
 print(f"Lenght:{len(test_dataloader)} batches of {BATCH_SIZE}...")
 print("\n")
 
-# Create a flatten layer
 flatten_model = nn.Flatten()
 
-# Setup device agnostic code
 device = "cuda" if torch.cuda.is_available() else "cpu"
 print(f"Using device: {device}")
 
-# Create a model with non-linear and linear layers # page 105
-
 class FashionMNISTModelV1(nn.Module):
-    def __init__(self, input_shape:int, hidden_units:int, output_shape:int):
+    def __init__(self, input_shape:int,
+                  hidden_units:int, 
+                  output_shape:int):
         super().__init__()
         self.layer_stack = nn.Sequential(
             nn.Flatten(),
@@ -110,55 +98,56 @@ class FashionMNISTModelV1(nn.Module):
     def forward(self, x):
         return self.layer_stack(x)
 
-# Create an instance of model_1
 torch.manual_seed(42)
-model_1 = FashionMNISTModelV1(
-    input_shape=28*28, # this is 784
-    hidden_units=10, # how many uints in hidden layer
-    output_shape=len(class_names) # how many uints in output layer
-).to(device)
-    
-loss_fn = nn.CrossEntropyLoss() # measure how wrong our model is
-optimizer = torch.optim.SGD(params=model_1.parameters(), # tries to update our model's parameters to reduce the loss 
-                            lr=0.1)
+model_1 = FashionMNISTModelV1(input_shape=784, hidden_units=10, output_shape=len(class_names)).to(device) 
+next(model_1.parameters()).device
 
-## training function
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(params=model_1.parameters(), lr=0.1)
+
 def train_step(model: torch.nn.Module,
                data_loader: torch.utils.data.DataLoader,
                loss_fn: torch.nn.Module,
-               optimizer: torch.optim.Optimizer, 
+               optimizer: torch.optim.Optimizer,
                accuracy_fn,
                device: torch.device = device):
-    """ Performs a training with model trying to learn on data_loader. """
-    train_loss, train_acc = 0, 0
-    model.train()
-    for X, y in tqdm(enumerate(data_loader)):
-        ## Send the data to the target device
-            X, y = X.to(device), y.to(device)
-        ## 1. Forward pass
-            y_pred = model(X)
-        ## 2. Calcurate the loss (per batch)
-            loss = loss_fn(y_pred, y)
-            train_loss += loss
-        ## Calculate loss and accuracy
-            train_acc += accuracy_fn(y_true=y, 
-                               y_pred=y_pred.argmax(dim=1))   
-        ## 3. optimzer zero gradient 
-            optimizer.zero_grad()
-        ## 4. Loss backward    
-            loss.backward()
-        ## 5. optimzer step (update the model's parameters once *per batch*)
-            optimizer.step()
+  """Performs a training with model trying to learn on data_loader."""
+  train_loss, train_acc = 0, 0
+
+  # Put model into training mode
+  model.train()
+
+  # Add a loop to loop through the training batches
+  for batch, (X, y) in enumerate(data_loader):
+    # Put data on target device 
+    X, y = X.to(device), y.to(device)
+
+    # 1. Forward pass (outputs the raw logits from the model)
+    y_pred = model(X)
     
-    ### Divide total train loss and acc by length of train dataloader
-    train_loss /= len(data_loader)
-    train_acc /= len(data_loader)
-    print(f"Train loss: {train_loss:.5f} | Train acc: {train_acc:.2f}%")
+    # 2. Calculate loss and accuracy (per batch)
+    loss = loss_fn(y_pred, y)
+    train_loss += loss # accumulate train loss
+    train_acc += accuracy_fn(y_true=y,
+                             y_pred=y_pred.argmax(dim=1)) # go from logits -> prediction labels
+    
+    # 3. Optimizer zero grad
+    optimizer.zero_grad()
+    
+    # 4. Loss backward
+    loss.backward()
+    
+    # 5. Optimizer step (update the model's parameters once *per batch*)
+    optimizer.step()
+  
+  # Divide total train loss and acc by length of train dataloader
+  train_loss /= len(data_loader)
+  train_acc /= len(data_loader)
+  print(f"Train loss: {train_loss:.5f} | Train acc: {train_acc:.2f}%")
 
 def test_step(model: torch.nn.Module,
               data_loader: torch.utils.data.DataLoader, 
               loss_fn: torch.nn.Module,
-              optimizer: torch.optim.Optimizer,
               accuracy_fn,
               device: torch.device = device):
   """Performs a testing loop step on model going over data_loader."""
@@ -211,6 +200,3 @@ train_time_end_on_gpu = timer()
 total_train_time_model_1 = print_train_time(start=train_time_start_on_gpu,
                                             end=train_time_end_on_gpu,
                                             device=device)
-
-
-
