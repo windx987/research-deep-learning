2492debbd19fa60b513e5ab3aa7c39ecbf3d5f63
teerawat-cc <terawat.cc@gmail.com>
2023-03-24 21:54:43 +0700

    first commit

---
+++ Learn PyTorch/03_pytorch_computer_vision/02_MNIST_exercises.py
@@ -0,0 +1,20 @@
+    # 1. What are 3 areas in industry where computer vision is currently being used?
+# answer >> 1. self driving car 
+#           2. pick-place product 
+#           3. authorization face recognition
+
+# 2. Search "what is overfitting in machine learning" and write down a sentence about what you find.
+# answer >> Overfitting is an undesirable machine learning behavior that occurs 
+#           when the machine learning model gives accurate predictions for training data but not for new data
+
+# 3. Search "ways to prevent overfitting in machine learning", write down 3 of the things you find and a sentence about each.
+# answer >> You can prevent overfitting by diversifying and scaling your training data set or using some other data science strategies, like those given below.
+            # 1. Early stopping
+            # 2. Pruning
+            # 3. Regularization
+            # 4. Ensembling
+            # 5. Data augmentation
+
+# 4. Spend 20-minutes reading and clicking through the CNN Explainer website.
+
+
