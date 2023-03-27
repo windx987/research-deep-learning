29424e142bcaac6414fe1ea9779e6569b3392bd0
teerawat-cc <terawat.cc@gmail.com>
2023-03-27 20:39:00 +0700

    first commit

---
+++ Learn PyTorch/04_pytorch_custom_datasets/dataset.py
@@ -0,0 +1,43 @@
+import torch 
+from torch import nn
+
+# Note: PyTorch 1.10.0+ is required for this course
+print(torch.__version__)
+
+# Setup device-agnostic code
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print(device)
+
+import requests
+import zipfile
+from pathlib import Path
+
+# Setup path to a data folder
+data_path = Path("data/")
+image_path = data_path / "pizza_steak_sushi"
+
+# If the image folder doesn't exist, download it and prepare it...
+if image_path.is_dir():
+  print(f"{image_path} directory already exists... skipping download")
+else: 
+  print(f"{image_path} does not exist, creating one...")
+  image_path.mkdir(parents=True, exist_ok=True)
+
+# Download pizza, steak and suhsi data
+with open(data_path / "pizza_steak_sushi.zip", "wb") as f:
+  request = requests.get("https://github.com/mrdbourke/pytorch-deep-learning/raw/main/data/pizza_steak_sushi.zip")
+  print("Downloading pizza, steak, suhsi data...")
+  f.write(request.content)
+
+# Unzip pizza, steak, sushi data
+with zipfile.ZipFile(data_path / "pizza_steak_sushi.zip", "r") as zip_ref:
+  print("Unzipping pizza, steak and sushi data...")
+  zip_ref.extractall(image_path)
+
+import os
+def walk_through_dir(dir_path):
+  """Walks through dir_path returning its contents."""
+  for dirpath, dirnames, filenames in os.walk(dir_path):
+    print(f"There are {len(dirnames)} directories and {len(filenames)} images in '{dirpath}'.")
+
+walk_through_dir(image_path)
