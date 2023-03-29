e356c19be91187bb13259aeb08d5dce1d67e6a33
teerawat-cc <terawat.cc@gmail.com>
2023-03-30 02:54:13 +0700

    update visualizing image

--- Learn PyTorch/04_pytorch_custom_datasets/dataset.py
+++ Learn PyTorch/04_pytorch_custom_datasets/dataset.py
@@ -16,23 +16,23 @@ from pathlib import Path
 data_path = Path("data/")
 image_path = data_path / "pizza_steak_sushi"
 
-# If the image folder doesn't exist, download it and prepare it...
-if image_path.is_dir():
-  print(f"{image_path} directory already exists... skipping download")
-else: 
-  print(f"{image_path} does not exist, creating one...")
-  image_path.mkdir(parents=True, exist_ok=True)
+# # If the image folder doesn't exist, download it and prepare it...
+# if image_path.is_dir():
+#   print(f"{image_path} directory already exists... skipping download")
+# else: 
+#   print(f"{image_path} does not exist, creating one...")
+#   image_path.mkdir(parents=True, exist_ok=True)
 
-# Download pizza, steak and suhsi data
-with open(data_path / "pizza_steak_sushi.zip", "wb") as f:
-  request = requests.get("https://github.com/mrdbourke/pytorch-deep-learning/raw/main/data/pizza_steak_sushi.zip")
-  print("Downloading pizza, steak, suhsi data...")
-  f.write(request.content)
+# # Download pizza, steak and suhsi data
+# with open(data_path / "pizza_steak_sushi.zip", "wb") as f:
+#   request = requests.get("https://github.com/mrdbourke/pytorch-deep-learning/raw/main/data/pizza_steak_sushi.zip")
+#   print("Downloading pizza, steak, suhsi data...")
+#   f.write(request.content)
 
-# Unzip pizza, steak, sushi data
-with zipfile.ZipFile(data_path / "pizza_steak_sushi.zip", "r") as zip_ref:
-  print("Unzipping pizza, steak and sushi data...")
-  zip_ref.extractall(image_path)
+# # Unzip pizza, steak, sushi data
+# with zipfile.ZipFile(data_path / "pizza_steak_sushi.zip", "r") as zip_ref:
+#   print("Unzipping pizza, steak and sushi data...")
+#   zip_ref.extractall(image_path)
 
 import os
 def walk_through_dir(dir_path):
@@ -47,3 +47,28 @@ train_dir = image_path / "train"
 test_dir = image_path / "test"
 
 print(train_dir, test_dir)
+
+import random 
+from PIL import Image
+
+# Set seed
+# random.seed(42)
+
+# 1. Get all image paths 
+image_path_list = list(image_path.glob("*/*/*.jpg"))
+
+# 2. Pick a random image path
+random_image_path = random.choice(image_path_list)
+
+# 3. Get image class from path name (the image class is the name of the directory where the image is stored)
+image_class = random_image_path.parent.stem
+
+# 4. Open image
+img = Image.open(random_image_path)
+
+# 5. Print metadata 
+print(f"Random image path: {random_image_path}")
+print(f"Image class: {image_class}")
+print(f"Image height: {img.height}")
+print(f"Image width: {img.width}")
+img.show()
