1bb4259419f0503ca9498821eb354a65b90758f0
teerawat-cc <terawat.cc@gmail.com>
2023-03-31 01:15:51 +0700

    update learn tensorflow and pytorch folder

--- Learn PyTorch/04_pytorch_custom_datasets/dataset.py
+++ Learn PyTorch/04_pytorch_custom_datasets/dataset.py
@@ -66,9 +66,70 @@ image_class = random_image_path.parent.stem
 # 4. Open image
 img = Image.open(random_image_path)
 
-# 5. Print metadata 
-print(f"Random image path: {random_image_path}")
-print(f"Image class: {image_class}")
-print(f"Image height: {img.height}")
-print(f"Image width: {img.width}")
-img.show()
+# # 5. Print metadata 
+# print(f"Random image path: {random_image_path}")
+# print(f"Image class: {image_class}")
+# print(f"Image height: {img.height}")
+# print(f"Image width: {img.width}")
+# img.show()
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Turn the image into an array
+img_as_array = np.asarray(img)
+
+print(img_as_array)
+
+# Plot the image with matplotlib
+plt.figure(figsize=(10, 7))
+plt.imshow(img_as_array)
+plt.title(f"Image class: {image_class} | Image shape: {img_as_array.shape} -> [height, width, color_channels] (HWC)")
+plt.axis(False)
+plt.show()
+
+
+# wating 100
+
+import torch
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+
+# Write a transform for image
+data_transform = transforms.Compose([
+  # Resize our images to 64x64
+  transforms.Resize(size=(64, 64)),
+  # Flip the images randomly on the horizontal
+  transforms.RandomHorizontalFlip(p=0.5),
+  # Turn the image into a torch.Tensor
+  transforms.ToTensor() 
+])
+
+def plot_transformed_images(image_paths: list, transform, n=3, seed=None):
+  """
+  Selects random images from a path of images and loads/transforms 
+  them then plots the original vs the transformed version.
+  """
+  if seed:
+    random.seed(seed)
+  random_image_paths = random.sample(image_paths, k=n)
+  for image_path in random_image_paths:
+    with Image.open(image_path) as f:
+      fig, ax = plt.subplots(nrows=1, ncols=2)
+      ax[0].imshow(f)
+      ax[0].set_title(f"Original\nSize: {f.size}")
+      ax[0].axis(False)
+
+      # Transform and plot target image
+      transformed_image = transform(f).permute(1, 2, 0) # note we will need to change shape for matplotlib (C, H, W) -> (H, W, C)
+      ax[1].imshow(transformed_image)
+      ax[1].set_title(f"Transformed\nShape: {transformed_image.shape}")
+      ax[1].axis("off")
+
+      fig.suptitle(f"Class: {image_path.parent.stem}", fontsize=16)
+
+plot_transformed_images(image_paths=image_path_list,
+                        transform=data_transform,
+                        n=3,
+                        seed=None)
+
